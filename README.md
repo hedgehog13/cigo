652f8d015a3aa9fe35199836068c426ce4e3da13
hedgehog13 <ira.varshavsky@gmail.com>
2020-10-25 14:58:08 -0400

    prod

---
+++ README.md
@@ -0,0 +1,30 @@
+This is a Cigo-tracker test application
+
+Pre requirements: install nodejs https://nodejs.org/en/download
+
+## git link
+https://github.com/hedgehog13/cigo
+Download from git
+App folder includes 2 sub folders client & server
+App contains mysql script to create db
+
+
+
+## db 
+I put the db on google cloud
+the IP configured in server/connection.js
+if you decide to run locally, please update server/connection.js host and port properties
+
+## Local environment installation
+
+cd server
+    npm install 
+    npm start
+cd client
+    npm install
+    npm start    
+
+
+## heroku link
+    https://cigo-dev.herokuapp.com/
+
--- client/src/app/orders/orders-list/orders-list.component.ts
+++ client/src/app/orders/orders-list/orders-list.component.ts
@@ -1,17 +1,18 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {OrdersService} from "../orders.service";
 import {ConfirmationDialogService} from "../confirmation-dialog/confirmation-dialog.service";
-
+import {MessageService, PrimeNGConfig} from "primeng/api";
 
 @Component({
   selector: 'app-orders-list',
   templateUrl: './orders-list.component.html',
-  styleUrls: ['./orders-list.component.css']
+  styleUrls: ['./orders-list.component.css'],
+  providers: [MessageService, PrimeNGConfig]
 })
 export class OrdersListComponent implements OnInit, OnDestroy {
 
   ordersListSub$;
-  isLoading$;
+
   status = [{name: 'Pending', value: 0},
     {name: 'Assigned', value: 1}, {name: 'On Route', value: 2}, {name: 'Done', value: 3}, {
       name: 'Cancelled',
@@ -19,22 +20,27 @@ export class OrdersListComponent implements OnInit, OnDestroy {
     }];
 
 
-  constructor(private ordersSrv: OrdersService, private confirmationDialogService: ConfirmationDialogService) {
+  constructor(private ordersSrv: OrdersService,
+              private messageService: MessageService, private primengConfig: PrimeNGConfig,
+              private confirmationDialogService: ConfirmationDialogService) {
   }
 
 
   ngOnInit() {
+    this.primengConfig.ripple = true;
     this.ordersListSub$ = this.ordersSrv.getOrdersData().subscribe();
     this.ordersSrv.orderId$.subscribe(() => {
       this.ordersListSub$ = this.ordersSrv.getOrdersData();
     })
-    this.ordersSrv.isLoadnig$.subscribe(res => {
-      this.isLoading$ =res;
-    });
-  }
 
+  }
+  showSuccess(message) {
+    this.messageService.add({severity: 'success', summary: 'Success', detail: `${message}`});
+  }
   updateAction(id, e) {
-    this.ordersSrv.updateStatus(id, e.target.value).subscribe();
+    this.ordersSrv.updateStatus(id, e.target.value).subscribe(()=>{
+      this.showSuccess('Order has been updated');
+    });
   }
 
   deleteOrder(order_id) {
@@ -47,7 +53,7 @@ export class OrdersListComponent implements OnInit, OnDestroy {
     this.confirmationDialogService.confirm('Please confirm..', 'Do you really want to ... ?')
       .then((confirmed) => {
         if (confirmed) {
-          this.ordersSrv.removeOrder(order_id).subscribe();
+          this.ordersSrv.removeOrder(order_id).subscribe(()=>this.showSuccess('Order has been deleted'));
         }
       }).catch(() => console.log('User dismissed the dialog (e.g., by using ESC, clicking the cross icon, or clicking outside the dialog)'));
   }
